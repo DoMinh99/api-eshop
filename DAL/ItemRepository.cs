984fe50d215092e1d3a6e3d35bae54c607c56f6c
dohongminh2811999@gmail.com <57739580+DoMinh99@users.noreply.github.com>
2020-10-24 00:00:21 +0700

    Add project files.

---
+++ DAL/ItemRepository.cs
@@ -0,0 +1,97 @@
+﻿using DAL.Helper;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL
+{
+    public partial class ItemRepository : IItemRepository
+    {
+        private IDatabaseHelper _dbHelper;
+        public ItemRepository(IDatabaseHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+
+        public bool Create(ItemModel model)
+        {
+            string msgError = "";
+            try
+            {
+                var result = _dbHelper.ExecuteScalarSProcedureWithTransaction(out msgError, "sp_item_create",
+                "@id", model.product_id,
+                "@product_id", model.product_id,
+                "@product_name", model.product_name,
+                "@brand_id", model.brand_id,
+                "@category_id", model.category_id,
+                "@model_year", model.model_year,
+                "@list_price", model.list_price,
+                "@Image", model.Image,
+                "@description", model.description,
+                "@specifications", model.specifications);
+                if ((result != null && !string.IsNullOrEmpty(result.ToString())) || !string.IsNullOrEmpty(msgError))
+                {
+                    throw new Exception(Convert.ToString(result) + msgError);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public ItemModel GetDatabyID(int id)
+        {
+            string msgError = "";
+            try
+            {
+                var dt = _dbHelper.ExecuteSProcedureReturnDataTable(out msgError, "sp_product_get_by_id",
+                     "@id", id);
+                if (!string.IsNullOrEmpty(msgError))
+                    throw new Exception(msgError);
+                return dt.ConvertTo<ItemModel>().FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public List<ItemModel> GetDataAll()
+        {
+            string msgError = "";
+            try
+            {
+                var dt = _dbHelper.ExecuteSProcedureReturnDataTable(out msgError, "sp_product_all");
+                if (!string.IsNullOrEmpty(msgError))
+                    throw new Exception(msgError);
+                return dt.ConvertTo<ItemModel>().ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public List<ItemModel> Search(int pageIndex, int pageSize, out long total, string item_group_id)
+        {
+            string msgError = "";
+            total = 0;
+            try
+            {
+                var dt = _dbHelper.ExecuteSProcedureReturnDataTable(out msgError, "sp_item_search",
+                    "@page_index", pageIndex,
+                    "@page_size", pageSize,
+                    "@item_group_id", item_group_id);
+                if (!string.IsNullOrEmpty(msgError))
+                    throw new Exception(msgError);
+                if (dt.Rows.Count > 0) total = (long)dt.Rows[0]["RecordCount"];
+                return dt.ConvertTo<ItemModel>().ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Model/ItemModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public class ItemModel
+    {
+        public int product_id { get; set; }
+        public string product_name { get; set; }
+        public int brand_id { get; set; }
+        public int category_id { get; set; }
+        public DateTime model_year { get; set; }
+        public decimal list_price { get; set; }
+        public string Image { get; set; }
+        public string description { get; set; }
+
+        public string specifications { get; set; }
+    }
+}
